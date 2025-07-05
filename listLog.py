8a9027199c3bb2eb4e782328dff1d68cb42254d8
KIP1SH-N <legittic@gmail.com>
2025-07-05 17:37:54 +0500

    Delete listLog

--- listLog.py
+++
@@ -1,7 +0,0 @@
-import base64
-from cryptography.fernet import Fernet
-def listLog(f,cip):
-    file=f.read()
-    fileDecode=base64.b64decode(file)
-    decrypted=cip.decrypt(fileDecode)
-    print(decrypted.decode())
