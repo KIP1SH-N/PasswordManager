da97bf8cf59867876b4ea6a5bdc57ccad2fc13c1
KIP1SH-N <legittic@gmail.com>
2025-07-06 11:27:09 +0500

    full version

--- delLog.py
+++ delLog.py
@@ -4,6 +4,7 @@ import base64
 def delLog(f,cip):
     findDel=str(input("Login: "))
     f.seek(0)
+    colle={}
     for line in f:
         line=line.strip()
         if not line:
@@ -11,6 +12,15 @@ def delLog(f,cip):
         fileDecode=base64.b64decode(line)
         decrypted=cip.decrypt(fileDecode)       
         a=decrypted.decode()
-        b=ast.literal_eval(a)
-        if b.keys == findDel:
-            del(b[finDel])
+        b=ast.literal_eval(a)        
+        colle.update(b)
+    del(colle[findDel])
+    f.seek(0)
+    f.truncate()
+    colleStr=str(colle)
+    fileEncrypted=cip.encrypt(colleStr.encode())
+    fileBase64=base64.b64encode(fileEncrypted).decode()
+    f.write(fileBase64 + "\n")
+    print("Success")
+    
+    
